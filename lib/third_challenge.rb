036bad2c21a6410fd3622674e4a9e5ad31053f72
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 08:42:12 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,15 +8,21 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Montague",
+        age: "54"
         },
       :hero => {
         #your key/value pairs here
+        name: "Romeo",
+        age: "15",
+        status: 'alive'
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        
         },
       :matriarch => {
         #your key/value pairs here
